a9d54d52d3a753e2221d327291d3e5deeecd1935
krish-vijayan <krishvijayan55@gmail.com>
2022-06-28 10:39:31 -0400

    Initial commit
    Added weather-app

---
+++ src/components/searchLocations.js
@@ -0,0 +1,17 @@
+import { useState } from 'react';
+
+function searchLocation() {
+
+
+    return (
+        <div>
+            <select>
+                <option value="toronto">Toronto</option>
+                <option value="new-jersey">New Jersey</option>
+                <option value="mumbai">Mumbai</option>
+            </select>
+        </div>
+    );
+}
+
+export default searchLocation();
