1ec5c3503e4ff1fc7b17607391a9d6b7c22eda48
krish-vijayan <krishvijayan55@gmail.com>
2022-07-22 12:47:56 -0400

    Fixed Error Handling

--- src/components/main-screen.js
+++ src/components/main-screen.js
@@ -1,23 +1,19 @@
 import '../App.css';
-import { useState, useContext, createContext } from 'react';
-import WeatherApi from './weather-api-call';
-
-
+import { useState} from 'react';
 
 function MainScreen() {
     return (
         <>
-            <background className="Background">
+           
             <div className="weather-app">
             <header className="Weather-app-header">
                 <h1 className="Title-font">Weather App</h1>
-                <img src="../images/clouds.png" className="Clouds" alt="cloud1" />
                 <label className='Location-label'>Krish, whats the weather in</label>
             </header>
                 <h3 className="Initials-font">By: Krish Vijayan</h3>
             
             </div>
-            </background>
+        
         </>
     )
 }
@@ -31,6 +27,7 @@ function InputCity() {
     const [ location, setLocation ]  = useState('');
     const [ comma, setComma ] = useState('');
     const [ blankCity, setBlankCity ] = useState('');
+    const [ validCity, setValidCity ] = useState('');
     const getData = (val) => {setCityName(val.target.value)};
 
   
@@ -40,12 +37,31 @@ function InputCity() {
                 placeholder="Enter City" className="Textbox"/>
 
                 <button className="InvisButton" onClick={() => {
-                    if (!cityName){ setBlankCity("Please Enter a City Name!"); return;}
-
+                    if (!cityName){ 
+                        setBlankCity("Please Enter a City Name!");
+                        setValidCity('');
+                        setTemp('');
+                        setDegree('');
+                        setCondition('');
+                        setLocation('');
+                        setComma(''); 
+                        return;}
                     else{
-                    setBlankCity(null);
-                    fetch (`http://api.weatherapi.com/v1/current.json?key=a181a7a9bd3248d481e155948222906&q=${cityName}&aqi=no`)
-                    .then((res) => res.json())
+                    setBlankCity('');
+                    fetch (`https://api.weatherapi.com/v1/current.json?key=a181a7a9bd3248d481e155948222906&q=${cityName}&aqi=no`)
+                    .then((res) =>{
+                        if (res.status !== 200) {
+                            setTemp('');
+                            setDegree('');
+                            setCondition('');
+                            setLocation('');
+                            setComma('');
+                            setValidCity("Please Enter a Valid City Name!")
+                            throw new Error("Not 200 Response")}
+                        else {
+                            setValidCity('');
+                            return res.json()}
+                    })
                     .then((data) => {
                         setTemp(data.current);
                         setDegree("°C");
@@ -56,13 +72,14 @@ function InputCity() {
                     })
                     .catch(error => console.log(`Hey, the error is ${error}`));
                 }}}>
-                    <img className="onHover Search" src="../images/search.png"/>
+                    <img className="onHover Search" src="../images/search.png" alt=""/>
                 </button>  
                 <h1 className="Location">{location.name}{comma} {location.country}</h1>
                 <h1 className="Temperature">{temp.temp_c}{degree}</h1>
-                <img className="ConditionIcon" src={condition.icon}/>
+                <img className="ConditionIcon" src={condition.icon} />
                 <h1 className="ConditionText">{condition.text}</h1> 
                 <h1 className="Location">{blankCity}</h1>
+                <h1 className="Location">{validCity}</h1>
               
         </> 
     )
