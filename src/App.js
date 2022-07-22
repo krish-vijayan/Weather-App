9c487d87a4096c5ed45c42d0332f27fa00f6f701
krish-vijayan <krishvijayan55@gmail.com>
2022-07-22 12:56:54 -0400

    Fixed Error Handling

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import {MainScreen, InputCity} from './components/main-screen';
+import {Helmet} from "react-helmet";
 
 
 
 function App() {
   return (
     <>
-    <MainScreen/>
-    <InputCity/>
+    <Helmet>
+            <meta charSet="utf-8" />
+            <title>Krish's Weather App</title>
+            <link rel="canonical" href="http://mysite.com/example" />
+            <meta name="description" content="Weather application" />
+
+    </Helmet>
+    
+      <background className="Background"/>
+    <div className="Container">
+      <MainScreen/>
+      <InputCity/>
+    </div>
+   
+      
     </>
     
   );
