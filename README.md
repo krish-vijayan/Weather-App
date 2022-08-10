c2e3d573a4aa7b5c6350223021c6b791166f9e5f
krish-vijayan <krishvijayan55@gmail.com>
2022-08-09 23:47:09 -0400

    Fixed README

--- README.md
+++ README.md
@@ -1,11 +1,7 @@
 # weather-app
 
-This is a weather application created by using React and a weather api from https://www.weatherapi.com.
+This is a weather application created by using React JS and a weather api from https://www.weatherapi.com.
 
-You can search the weather of pretty much any city around the world, and the current temperature and condition should display and be fairly accurate! 
-
-Currently, I have not learned CSS properly and hence the application will look different depending on the 
-resolution size of your screen. Therefore, a temporary fix for this would be to use CRTL +/- to scale the 
-display properly. 
+You can search the weather of pretty much any city around the world, and the current temperature and condition should display and be fairly accurate!
 
 Finally, here is a link to the application: https://krish-vijayan-weather-app.netlify.app/.
--- src/components/main-screen.js
+++ src/components/main-screen.js
@@ -1,101 +1,106 @@
-import '../App.css';
-import { useState} from 'react';
-import { useOnKeyPress } from './onKeyPress';
+import "../App.css";
+import { useState } from "react";
+import { useOnKeyPress } from "./onKeyPress";
 
 function MainScreen() {
-    return (
-        <>
-           
-            <div className="weather-app">
-            <header className="Weather-app-header">
-                <h1 className="Title-font">Weather App</h1>
-                <label className='Location-label'>Krish, what's the weather in</label>
-            </header>
-                <h3 className="Initials-font">By: Krish Vijayan</h3>
-            
-            </div>
-        
-        </>
-    )
+  return (
+    <>
+      <div className="weather-app">
+        <header className="Weather-app-header">
+          <h1 className="Title-font">Weather App</h1>
+          <label className="Location-label">Krish, what's the weather in</label>
+        </header>
+        <h3 className="Initials-font">By: Krish Vijayan</h3>
+      </div>
+    </>
+  );
 }
-
 
 function InputCity() {
-    const [ cityName, setCityName ] = useState('');
-    const [ temp, setTemp ] = useState('');
-    const [ degree, setDegree] = useState('');
-    const [ condition, setCondition ] = useState('');
-    const [ location, setLocation ]  = useState('');
-    const [ comma, setComma ] = useState('');
-    const [ blankCity, setBlankCity ] = useState('');
-    const [ validCity, setValidCity ] = useState('');
-    const [ icon, setIcon ] = useState(null);
-    const getData = (val) => {setCityName(val.target.value)};
+  const [cityName, setCityName] = useState("");
+  const [temp, setTemp] = useState("");
+  const [degree, setDegree] = useState("");
+  const [condition, setCondition] = useState("");
+  const [location, setLocation] = useState("");
+  const [comma, setComma] = useState("");
+  const [blankCity, setBlankCity] = useState("");
+  const [validCity, setValidCity] = useState("");
+  const [icon, setIcon] = useState(null);
+  const getData = (val) => {
+    setCityName(val.target.value);
+  };
 
-    const apiHandler = () => {
-        if (!cityName){ 
-            setBlankCity("Please Enter a City Name!");
-            setValidCity('');
-            setTemp('');
-            setDegree('');
-            setCondition('');
-            setIcon('');
-            setLocation('');
-            setComma(''); 
-            return;
-        }
-        else{
-        setBlankCity('');
-        fetch (`https://api.weatherapi.com/v1/current.json?key=a181a7a9bd3248d481e155948222906&q=${cityName}&aqi=no`)
-        .then((res) =>{
-            if (res.status !== 200) {
-                setTemp('');
-                setDegree('');
-                setCondition('');
-                setIcon('');
-                setLocation('');
-                setComma('');
-                setValidCity("Please Enter a Valid City Name!")
-                throw new Error("Not 200 Response")}
-            else {
-                setValidCity('');
-                return res.json()}
+  const apiHandler = () => {
+    if (!cityName) {
+      setBlankCity("Please Enter a City Name!");
+      setValidCity("");
+      setTemp("");
+      setDegree("");
+      setCondition("");
+      setIcon("");
+      setLocation("");
+      setComma("");
+      return;
+    } else {
+      setBlankCity("");
+      fetch(
+        `https://api.weatherapi.com/v1/current.json?key=a181a7a9bd3248d481e155948222906&q=${cityName}&aqi=no`
+      )
+        .then((res) => {
+          if (res.status !== 200) {
+            setTemp("");
+            setDegree("");
+            setCondition("");
+            setIcon("");
+            setLocation("");
+            setComma("");
+            setValidCity("Please Enter a Valid City Name!");
+            throw new Error("Not 200 Response");
+          } else {
+            setValidCity("");
+            return res.json();
+          }
         })
         .then((data) => {
-            setTemp(data.current);
-            setDegree("°C");
-            setCondition(data.current.condition);
-            setLocation(data.location);
-            setComma(",");
-            setIcon("ConditionIcon");
-            console.log("API WAS CALLED");
+          setTemp(data.current);
+          setDegree("°C");
+          setCondition(data.current.condition);
+          setLocation(data.location);
+          setComma(",");
+          setIcon("ConditionIcon");
+          console.log("API WAS CALLED");
         })
-        .catch(error => console.log(`Hey, the error is ${error}`));
+        .catch((error) => console.log(`Hey, the error is ${error}`));
     }
+  };
+  useOnKeyPress(apiHandler, "Enter");
+
+  return (
+    <>
+      <input
+        type="City"
+        onChange={getData}
+        placeholder="Enter City"
+        className="Textbox"
+      />
+
+      <button className="InvisButton" onClick={apiHandler}>
+        <img className="onHover Search" src="../images/search.png" alt="" />
+      </button>
+      <h1 className="Location">
+        {location.name}
+        {comma} {location.country}
+      </h1>
+      <h1 className="Temperature">
+        {temp.temp_c}
+        {degree}
+      </h1>
+      <img className={icon} src={condition.icon} />
+      <h1 className="ConditionText">{condition.text}</h1>
+      <h1 className="Location">{blankCity}</h1>
+      <h1 className="Location">{validCity}</h1>
+    </>
+  );
 }
-    useOnKeyPress(apiHandler, 'Enter');
-  
-    return ( 
-        <>
-                <input type="City" onChange={getData}
-                placeholder="Enter City" className="Textbox"/>
 
-                <button className="InvisButton" onClick={apiHandler}>
-                    <img className="onHover Search" src="../images/search.png" alt=""/>
-                </button>  
-                <h1 className="Location">{location.name}{comma} {location.country}</h1>
-                <h1 className="Temperature">{temp.temp_c}{degree}</h1>
-                <img className={icon} src={condition.icon} />
-                <h1 className="ConditionText">{condition.text}</h1> 
-                <h1 className="Location">{blankCity}</h1>
-                <h1 className="Location">{validCity}</h1>
-              
-        </> 
-    )
-}
-
-
-
-
-
-export  {MainScreen, InputCity};
+export { MainScreen, InputCity };
